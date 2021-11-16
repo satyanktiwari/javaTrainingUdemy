ec8ab134e104b179a841d98e31a49cd8271a7316
satyanktiwari <satyank.tiwari@gmail.com>
2021-11-16 16:34:19 +0530

    multiple changes to packages

---
+++ src/com/caveofprogramming/ocean/IndianOcean.java
@@ -0,0 +1,14 @@
+package com.caveofprogramming.ocean;
+/*
+ * To use classes from other packages these have to 
+ * imported using the import statement
+ */
+import com.caveofprogramming.plants.Seaweed;
+
+public class IndianOcean {
+	public static void main(String[] args) {
+		Seaweed seaweed1= new Seaweed();
+		seaweed1.info();
+	}
+
+}
