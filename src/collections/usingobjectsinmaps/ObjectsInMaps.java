8c79a24f6aa02114482f3f2bdbeb593c3d7b9ac2
satyanktiwari <satyank.tiwari@gmail.com>
2021-11-29 14:29:04 +0530

    completed objects in maps and set

--- src/collections/usingobjectsinmaps/ObjectsInMaps.java
+++ src/collections/usingobjectsinmaps/ObjectsInMaps.java
@@ -3,6 +3,7 @@ package collections.usingobjectsinmaps;
 import java.util.HashMap;
 import java.util.LinkedHashSet;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Set;
 
 class Person{
@@ -18,6 +19,23 @@ class Person{
 	public String toString() {
 		return "Person [id=" + id + ", name=" + name + "]";
 	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id, name);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Person other = (Person) obj;
+		return id == other.id && Objects.equals(name, other.name);
+	}
 	
 	
 }
@@ -27,25 +45,26 @@ public class ObjectsInMaps {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		Person p1 = new Person(1, "Bob");
-		Person p1 = new Person(1, "Bob");
-		Person p1 = new Person(1, "Bob");
-		Person p1 = new Person(1, "Bob");
+		Person p1 = new Person(0, "Bob");
+		Person p2 = new Person(1, "Marley");
+		Person p3 = new Person(2, "Charlton");
+		Person p4 = new Person(1, "Marley");
 
-		Map<String, Integer> map = new HashMap<String, Integer>();
-		map.put("One", 1);
-		map.put("two", 2);
-		map.put("three", 3);
-		map.put("two", 2);
-		for (String key : map.keySet()) {
+		Map<Person, Integer> map = new HashMap<Person, Integer>();
+		map.put(p1, 1);
+		map.put(p2, 2);
+		map.put(p3, 3);
+		map.put(p4, 1);
+		for (Person key : map.keySet()) {
 			System.out.println(key + " :" + map.get(key));
 		}
 		
-		Set<String> set = new LinkedHashSet<String>();
-		set.add("Cat");
-		set.add("mouse");
-		set.add("crow");
-		set.add("mouse");
+		Set<Person> set = new LinkedHashSet<Person>();
+		set.add(p1);
+		set.add(p2);
+		set.add(p3);
+		set.add(p4);
+		
 		System.out.println("\nElements in set are:\n"+set);
 	}
 
